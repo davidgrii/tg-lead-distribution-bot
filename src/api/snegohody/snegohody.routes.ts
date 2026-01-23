1da45ba1c1feacb0a9da1c24ee036aedfe50bec7
davidgrii <grikurovdavid@gmail.com>
2026-01-23 20:44:44 +0100

    update

--- src/api/snegohody/snegohody.routes.ts
+++ src/api/snegohody/snegohody.routes.ts
@@ -71,60 +71,61 @@ ${leadData}
 router.post(
   '/tilda-webhook-xlkja-snegohody-cart',
   async (req: Request, res: Response) => {
-    const lead = req.body as ISnegohodyRequest
-    const orConditions = []
-
-    const contactMethod = getContactMethod(lead)
-    const { contactPhone, telegramUsername } = getContactPhoneOrUsername(lead)
-
-    if (contactPhone !== '') {
-      orConditions.push({ phone: contactPhone })
-    } else {
-      orConditions.push({ telegram_username: telegramUsername })
-    }
-
-    const client = await ClientsModel.findOne({
-      $or: orConditions
-    })
-
-    if (!client) {
-      await ClientsModel.create({
-        name: lead.Name,
-        contact_method: contactMethod,
-        phone: contactPhone,
-        telegram_username: telegramUsername
-      })
-    }
-
-    console.log('NEW LEAD snegohody:', lead)
-
-    const leadData = Object.entries(lead)
-      .map(([key, value], index) => {
-        const formatted = `<b>${key?.at(0)?.toUpperCase() + key.slice(1)}:</b> — ${value}`
-        return (index + 1) % 3 === 0 ? formatted + '\n' : formatted
-      })
-      .join('\n')
-
-    const message = `
-  ❗️ <b>Получена новая заявка:</b> ❗️ 
-    
-<b>От:</b> <code>${req.host}</code>
-<b>Источник:</b> <code>Корзина</code>
-  
-${leadData}
-  `
-
-    await bot.api.sendMessage(
-      CHANNELS_SNEGOHOD[channelIndexSnegohody],
-      message,
-      {
-        parse_mode: 'HTML'
-      }
-    )
-
-    channelIndexSnegohody =
-      (channelIndexSnegohody + 1) % CHANNELS_SNEGOHOD.length
-    res.sendStatus(200)
+    return res.sendStatus(200)
+//     const lead = req.body as ISnegohodyRequest
+//     const orConditions = []
+//
+//     const contactMethod = getContactMethod(lead)
+//     const { contactPhone, telegramUsername } = getContactPhoneOrUsername(lead)
+//
+//     if (contactPhone !== '') {
+//       orConditions.push({ phone: contactPhone })
+//     } else {
+//       orConditions.push({ telegram_username: telegramUsername })
+//     }
+//
+//     const client = await ClientsModel.findOne({
+//       $or: orConditions
+//     })
+//
+//     if (!client) {
+//       await ClientsModel.create({
+//         name: lead.Name,
+//         contact_method: contactMethod,
+//         phone: contactPhone,
+//         telegram_username: telegramUsername
+//       })
+//     }
+//
+//     console.log('NEW LEAD snegohody:', lead)
+//
+//     const leadData = Object.entries(lead)
+//       .map(([key, value], index) => {
+//         const formatted = `<b>${key?.at(0)?.toUpperCase() + key.slice(1)}:</b> — ${value}`
+//         return (index + 1) % 3 === 0 ? formatted + '\n' : formatted
+//       })
+//       .join('\n')
+//
+//     const message = `
+//   ❗️ <b>Получена новая заявка:</b> ❗️
+//
+// <b>От:</b> <code>${req.host}</code>
+// <b>Источник:</b> <code>Корзина</code>
+//
+// ${leadData}
+//   `
+//
+//     await bot.api.sendMessage(
+//       CHANNELS_SNEGOHOD[channelIndexSnegohody],
+//       message,
+//       {
+//         parse_mode: 'HTML'
+//       }
+//     )
+//
+//     channelIndexSnegohody =
+//       (channelIndexSnegohody + 1) % CHANNELS_SNEGOHOD.length
+//     res.sendStatus(200)
   }
 )
 
--- src/api/spectehniki/spectehniki.routes.ts
+++ src/api/spectehniki/spectehniki.routes.ts
@@ -71,61 +71,62 @@ ${leadData}
 router.post(
   '/tilda-webhook-catalog-spectehniki-cart',
   async (req: Request, res: Response) => {
-    const lead = req.body as ISpectechnikiCartRequest
-    const orConditions = []
-
-    const contactMethod = getContactMethod(lead)
-    const { contactPhone, telegramUsername } = getContactPhoneOrUsername(lead)
-
-    if (contactPhone !== '') {
-      orConditions.push({ phone: contactPhone })
-    } else {
-      orConditions.push({ telegram_username: telegramUsername })
-    }
-
-    const client = await ClientsModel.findOne({
-      $or: orConditions
-    })
-
-    if (!client) {
-      await ClientsModel.create({
-        name: lead.Name,
-        contact_method: contactMethod,
-        phone: contactPhone,
-        telegram_username: telegramUsername
-      })
-    }
-
-    console.log('NEW LEAD spectehniki:', lead)
-    console.log(lead?.payment?.products)
-
-    const leadData = Object.entries(lead)
-      .map(([key, value], index) => {
-        const formatted = `<b>${key?.at(0)?.toUpperCase() + key.slice(1)}:</b> — ${value}`
-        return (index + 1) % 3 === 0 ? formatted + '\n' : formatted
-      })
-      .join('\n')
-
-    const message = `
-  ❗️ <b>Получена новая заявка:</b> ❗️ 
-    
-<b>От:</b> <code>${req.host}</code>
-<b>Источник:</b> <code>Корзина</code>
-  
-${leadData}
-  `
-
-    await bot.api.sendMessage(
-      CHANNELS_SPECTECHNIKI[channelIndexSpectehniki],
-      message,
-      {
-        parse_mode: 'HTML'
-      }
-    )
-
-    channelIndexSpectehniki =
-      (channelIndexSpectehniki + 1) % CHANNELS_SPECTECHNIKI.length
-    res.sendStatus(200)
+    return res.sendStatus(200)
+//     const lead = req.body as ISpectechnikiCartRequest
+//     const orConditions = []
+//
+//     const contactMethod = getContactMethod(lead)
+//     const { contactPhone, telegramUsername } = getContactPhoneOrUsername(lead)
+//
+//     if (contactPhone !== '') {
+//       orConditions.push({ phone: contactPhone })
+//     } else {
+//       orConditions.push({ telegram_username: telegramUsername })
+//     }
+//
+//     const client = await ClientsModel.findOne({
+//       $or: orConditions
+//     })
+//
+//     if (!client) {
+//       await ClientsModel.create({
+//         name: lead.Name,
+//         contact_method: contactMethod,
+//         phone: contactPhone,
+//         telegram_username: telegramUsername
+//       })
+//     }
+//
+//     console.log('NEW LEAD spectehniki:', lead)
+//     console.log(lead?.payment?.products)
+//
+//     const leadData = Object.entries(lead)
+//       .map(([key, value], index) => {
+//         const formatted = `<b>${key?.at(0)?.toUpperCase() + key.slice(1)}:</b> — ${value}`
+//         return (index + 1) % 3 === 0 ? formatted + '\n' : formatted
+//       })
+//       .join('\n')
+//
+//     const message = `
+//   ❗️ <b>Получена новая заявка:</b> ❗️
+//
+// <b>От:</b> <code>${req.host}</code>
+// <b>Источник:</b> <code>Корзина</code>
+//
+// ${leadData}
+//   `
+//
+//     await bot.api.sendMessage(
+//       CHANNELS_SPECTECHNIKI[channelIndexSpectehniki],
+//       message,
+//       {
+//         parse_mode: 'HTML'
+//       }
+//     )
+//
+//     channelIndexSpectehniki =
+//       (channelIndexSpectehniki + 1) % CHANNELS_SPECTECHNIKI.length
+//     res.sendStatus(200)
   }
 )
 
