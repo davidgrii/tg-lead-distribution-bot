2e0de0e84d4e48ee1297b99dae71ef8a3bcb0aa9
davidgrii <grikurovdavid@gmail.com>
2026-01-27 21:56:59 +0100

    update

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,10 +1,28 @@
-import {type Bot, Context} from "grammy";
+import {type Bot, Context, Keyboard} from "grammy";
 
 
 export const setupStartCommands = (bot: Bot) => {
   bot.command('start', async (ctx: Context) => {
-    return await ctx.reply('Working')
-  })
+    const username = ctx.from?.username || ''
+    const welcomeMessage =
+      `<b>👋 ${username}, Добро пожаловать!</b>
+
+<b>*Нажмите кнопку ниже*</b>
+чтобы поделиться контактом
+
+👇👇👇
+`;
+
+    const keyboard = new Keyboard()
+      .requestContact('☎️  ПОДЕЛИТЬСЯ КОНТАКТОМ  ☎️')
+      .resized()
+      .oneTime();
+
+    await ctx.reply(welcomeMessage, {
+      reply_markup: keyboard,
+      parse_mode: 'HTML'
+    });
+  });
 
   bot.command('chatid', async (ctx: Context) => {
     const chatId = String(ctx.chat?.id) || ''
